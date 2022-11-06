7347e6f0409c670618c77aa61c942a582a8b6601
Corey Powell <mistdragon100@gmail.com>
2022-11-06 09:06:52 -0500

    nokore_prelude: Add foundation_node_sounds as dep and setup node_sounds

--- mods/nokore_prelude/init.lua
+++ mods/nokore_prelude/init.lua
@@ -6,3 +6,9 @@ local mod = foundation.new_module("nokore_prelude", "0.3.0")
 mod.game = "nokore"
 mod.is_prelude = true
 mod.node_sounds = foundation.com.NodeSoundsRegistry:new()
+
+-- @namespace nokore
+nokore = rawget(_G, "nokore") or {}
+nokore.VERSION = assert(mod.VERSION)
+-- @alias node_sounds = nokore_prelude.node_sounds
+nokore.node_sounds = mod.node_sounds
