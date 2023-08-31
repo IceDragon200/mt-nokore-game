8fbbf1ac624a90a9fba3d55940714571589d56fb
Corey Powell <mistdragon100@gmail.com>
2023-08-31 10:07:38 -0500

    nokore_prelude: NodeSoundsRegistry now requires a mandatory name argument on creation

--- mods/nokore_prelude/init.lua
+++ mods/nokore_prelude/init.lua
@@ -5,10 +5,15 @@ local mod = foundation.new_module("nokore_prelude", "0.3.0")
 
 mod.game = "nokore"
 mod.is_prelude = true
-mod.node_sounds = foundation.com.NodeSoundsRegistry:new()
+mod.node_sounds = foundation.com.NodeSoundsRegistry:new("nokore_prelude:node_sounds_registry")
 
 -- @namespace nokore
 nokore = rawget(_G, "nokore") or {}
 nokore.VERSION = assert(mod.VERSION)
 -- @alias node_sounds = nokore_prelude.node_sounds
 nokore.node_sounds = mod.node_sounds
+
+-- register the "default" node sound set, usually just empty
+nokore.node_sounds:register_new("default", {})
+
+nokore.node_sounds:register_new("metal", {})
